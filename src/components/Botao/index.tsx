b0529d7125f23c575f72693c29eeb17d4be9cd15
Jean <cpjeanberg@gmail.com>
2022-06-02 12:57:12 -0300

    inicializando cap 3

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 class Botao extends React.Component{
     render() {
+        const color = 'red'
         return(
-            <button>
-                Botão
+            <button style={{ /* style={{}} é um atributo que não existe no HTML, mas é usado 
+        para criar estilos*/
+                backgroundColor: color
+            }}> 
+            Botão
             </button>
         )
     }
