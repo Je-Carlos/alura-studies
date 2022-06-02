76461f605cc58af400cafb67a8020834b0f9276c
Jean <cpjeanberg@gmail.com>
2022-06-02 19:41:29 -0300

    finalizando cap 4

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,34 +1,31 @@
-import React from "react";
-import style from'./Lista.module.scss';
+import React from 'react';
+import Item from './Item';
+import style from './Lista.module.scss';
 
 function Lista() {
-  const tarefas = [
-    {
-      tarefa: "React",
-      tempo: "02:00:00",
-    },
-    {
-      tarefa: "Javascript",
-      tempo: "01:00:00",
-    },
-    {
-      tarefa: "Typescript",
-      tempo: "03:00:00",
-    },
-  ];
+  const tarefas = [{
+    tarefa: 'React',
+    tempo: '02:00:00'
+  }, {
+    tarefa: 'Javascript',
+    tempo: '01:00:00'
+  }, {
+    tarefa: "Typescript",
+    tempo: "03:00:00"
+  }];
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do Dia </h2>
+      <h2> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className="item">
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+          <Item
+            key={index}
+            {...item}
+          />
         ))}
       </ul>
     </aside>
-  );
+  )
 }
 
 export default Lista;
