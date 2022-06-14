a622614a552b472024ea774aab7af40df7a806a1
Jean <cpjeanberg@gmail.com>
2022-06-14 13:09:51 -0300

    reaproveitando interface

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,12 +1,33 @@
-import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss';
+import { ITarefa } from "../../../types/tarefa";
+import style from "../Lista.module.scss";
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSeleciona: ITarefa) => void;
+}
 
-export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
-  console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
+export default function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id,
+  selecionaTarefa,
+}: Props) {
+  console.log("item atual: ", { tarefa, tempo, selecionado, completado, id });
   return (
-    <li className={style.item}>
+    <li
+      className={style.item}
+      onClick={() =>
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id
+        })
+      }
+    >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
-  )
+  );
 }
